2b639a25ab572a5c05523d2514bec69434af4920
feruzbekqahramov <feqahramnov@mail.com>
2023-12-24 15:25:26 +0500

    homework end

---
+++ index.js
@@ -0,0 +1,62 @@
+// 1-masala
+// function sum(arr) {
+//   let = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       sum += arr[i] ** 2;
+//     }
+//   }
+//   return sum;
+// }
+
+// 2-masala
+// let origin = ["false", "else", "reading", "uzb", "zzzz", "border", "else", "kar"];
+// let newArray = [];
+// for (var i = 0; i < origin.length; i++) {
+//   let newElement = origin[i][0];
+//   newArray.push(newElement);
+// }
+// console.log(newArray);
+
+// 3-masala
+// let array = [
+//   {
+//     age: 34,
+//   },
+//   {
+//     age: 15,
+//   },
+//   {
+//     age: 21,
+//   },
+//   {
+//     age: 76,
+//   },
+// ];
+// let sum = 1;
+// for (let i = 0; i < array.length; i++) {
+//   sum = sum * array[i].age;
+// }
+// console.log(sum);
+
+// 4-masala
+// function sum(arr) {
+//   let = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 2) {
+//       sum += arr[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// 5-masala
+// function sum(arr) {
+//   let = 50;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 1) {
+//       sum += arr[i];
+//     }
+//   }
+//   return sum;
+// }
